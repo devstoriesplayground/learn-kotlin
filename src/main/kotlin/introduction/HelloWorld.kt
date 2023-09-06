1acbfb3fd7e98385d83205c8d4b9aaf9fc40a261
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-06 15:52:52 +0800

    feat: add kotlin println function

---
+++ src/main/kotlin/introduction/HelloWorld.kt
@@ -0,0 +1,8 @@
+package introduction
+
+// The println() function prints the given message inside the quotation marks and newline to the standard output stream.
+// In this program, it prints Hello, World! and new line.
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
