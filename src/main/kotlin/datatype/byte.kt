8af141fcc83c4b679fd4357d26753c222486fa00
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-12 15:50:24 +0800

    chore: modification and add short data type

--- src/main/kotlin/datatype/byte.kt
+++ src/main/kotlin/datatype/byte.kt
@@ -3,7 +3,7 @@ package datatype
 // The Byte data type can have values from -128 to 127 (8-bit signed two's complement integer).
 // It is used instead of Int or other integer data types to save memory if it's certain that the value of a variable will be within [-128, 127]
 
-fun main(args : Array<String>) {
+fun main() {
     val range: Byte = 112
     println("$range")
 
--- src/main/kotlin/introduction/helloWorld.kt
+++ src/main/kotlin/introduction/helloWorld.kt
@@ -3,6 +3,6 @@ package introduction
 // The println() function prints the given message inside the quotation marks and newline to the standard output stream.
 // In this program, it prints Hello, World! and new line.
 
-fun main(args: Array<String>) {
+fun main() {
     println("Hello World!")
 }
