c0d2c2ab8396ecc2f0346da1abb268f0607848d7
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-14 16:33:16 +0800

    feat: add float, double and long data type

---
+++ src/main/kotlin/datatype/long.kt
@@ -0,0 +1,9 @@
+package datatype
+
+// The Long data type can have values from -263 to 263-1 (64-bit signed two's complement integer).
+
+fun main() {
+
+    val highestScore: Long = 9999
+    println("$highestScore")
+}
